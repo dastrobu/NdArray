8621e717742ca6e48da025cfe4564176fd38dfe1
Daniel Strobusch <1847260+dastrobu@users.noreply.github.com>
2022-12-08 10:44:27 +0100

    fix appendInterpolation compile errors

--- Sources/NdArray/string.swift
+++ Sources/NdArray/string.swift
@@ -27,17 +27,17 @@ fileprivate extension NdArray {
                 s.removeLast(2)
             }
         default:
-            let depthSeperator: String = String(repeating: separator, count: ndim - 1)
+            let depthSeparator: String = String(repeating: separator, count: ndim - 1)
             let a = NdArray(self)
             for i in 0..<shape[0] {
                 s += a[[Slice(i)]].string(separator: separator, indent: indent, depth: depth + 1, formatter: formatter)
                 s += ","
-                s += depthSeperator
+                s += depthSeparator
                 s += String(repeating: " ", count: indent * (depth + 1))
             }
             if shape[0] > 0 {
                 // remove trailing "," + "separators" + <whitespace>
-                s.removeLast(depthSeperator.count + 1 + indent * (depth + 1))
+                s.removeLast(depthSeparator.count + 1 + indent * (depth + 1))
             }
         }
         s += "]"
@@ -59,20 +59,20 @@ public extension String.StringInterpolation {
     ///               The closure is applied to each element to create the string representing the element.
     mutating func appendInterpolation<T>(_ value: NdArray<T>,
                                          style: StringFormat = .multiLine,
-                                         formatter: @escaping (T) -> String = {
-                                             "\($0)"
-                                         }) {
+                                         formatter: ((T) -> String)? = nil
+                                         ) {
 
+        let f = formatter ?? {element in "\(element)"}
         switch style {
         case .singleLine:
-            appendLiteral(value.string(separator: " ", indent: 0, formatter: formatter))
+            appendLiteral(value.string(separator: " ", indent: 0, formatter: f))
         case .multiLine:
             // first find the maximal field length for the items
             let n = value.reduce(1, { n, element in
-                Swift.max(n, formatter(element).count)
+                Swift.max(n, f(element).count)
             })
             let wrapperFormatter: (T) -> String = { element in
-                formatter(element).paddingToLeft(toLength: n, withPad: " ")
+                f(element).paddingToLeft(toLength: n, withPad: " ")
             }
             appendLiteral(value.string(separator: "\n", indent: 1, formatter: wrapperFormatter))
         }
